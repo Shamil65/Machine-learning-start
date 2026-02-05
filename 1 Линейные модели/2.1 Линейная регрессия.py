936b5d297d475ecc6f84db9bf20a720d94c4b534
Shamil65 <shamilziganshin132@gmail.com>
2026-02-05 20:22:37 +0500

    наконец-то доделал

--- 1 Линейные модели/2.1 Линейная регрессия.py
+++ 1 Линейные модели/2.1 Линейная регрессия.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 class MyLineReg():
-    def __init__(self, n_iter=100, learning_rate=0.01, weights=None):
+    def __init__(self, n_iter=100, learning_rate=0.001, weights=None):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
@@ -32,7 +32,9 @@ class MyLineReg():
         print("X после ввода нового столбца: \n", X, "\n")
         print("self.weights после заполнения: \n", self.weights, "\n")
         print("X_mat: \n", X_mat, "\n")
+        print("X_mat.T: \n", X_mat.T, "\n")
         print("y_vec: \n", y_vec, "\n")
+        print("m, n: \n", m, n, "\n")
         print("y_pred \n", y_pred, "\n")
         print("errors \n", errors, "\n")
         print("MSE \n", MSE, "\n")
@@ -43,33 +45,36 @@ class MyLineReg():
 
     def fit(self, X, y, verbose=False):
         X.insert(0, "bias", 1)
-        print(X)
+        m, n = X.shape # m - кол-во строк; n - кол-во столбцов
 
-        print(X.shape)
-        m, n = X.shape
-        print(n, m)
-
-        self.weights = np.ones(n)
-        print(self.weights)
-
-        X_mat = X.values
-        print(X_mat)
+        self.weights = np.ones(n) # засовывваем в эту переменную np массив со значениями 1 в кол-ве равном кол-ву переменных в одном векторе в X
+        
+        X_mat = X.values # Превращаем из DataFrame в np array
         y_vec = y.values
-        print(y_vec)
         y_pred = X_mat @ self.weights
+
         errors = y_pred - y_vec
-        errors_2 = errors**2
         MSE = np.mean(errors**2)
-        print(y_pred)
-        print(errors_2)
-        print(errors)
-        print(MSE)
+
+        if verbose:
+            print(f"start | loss: {MSE}")
 
         for i in range(1, self.n_iter + 1):
-            pass
-            # y_pred = X_mat @ self.weights
-            # errors = y_pred - y_vec
-            # print()       
+            y_pred = X_mat @ self.weights
+            errors = y_pred - y_vec           # 🔹 ОБЯЗАТЕЛЬНО обновляем
+            gradient = (2 / m) * (X_mat.T @ errors)
+            self.weights = self.weights - self.learning_rate * gradient
+            loss = np.mean(errors**2)         # теперь верный loss
+
+            if verbose and i % verbose == 0:
+                print(f"iter {i} | loss: {loss}")
+                print(self.weights)
+
+
+    def get_coef(self):
+        return self.weights[1:]
+
+
 
 
 X = pd.DataFrame({"x1": [3, 2],
@@ -80,4 +85,4 @@ y = pd.Series([0, 1])
 verbose = 10
 MyLineReg1 = MyLineReg()
 print(MyLineReg1)
-MyLineReg1.demo_fit(X, y, verbose)
+MyLineReg1.fit(X, y, verbose)
