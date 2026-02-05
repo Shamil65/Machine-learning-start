f11440b2bd27967407abaf2df37e030f1e8ff8a7
Shamil65 <shamilziganshin132@gmail.com>
2026-02-05 17:18:37 +0500

    Доделываю регрессию

--- 1 Линейные модели/2.1 Линейная регрессия.py
+++ 1 Линейные модели/2.1 Линейная регрессия.py
@@ -11,13 +11,33 @@ class MyLineReg():
         return "MyLineReg class: n_iter={}, learning_rate={}".format(self.n_iter, self.learning_rate)
     
     def demo_fit(self, X, y, verbose=False):
+
+        print("X до ввода нового столбца: \n", X, "\n")
         X.insert(0, "bias", 1)
         m, n = X.shape # m - кол-во строк; n - кол-во столбцов
+
+        print("self.weights до заполнения: \n", self.weights, "\n")
         self.weights = np.ones(n) # засовывваем в эту переменную np массив со значениями 1 в кол-ве равном кол-ву переменных в одном векторе в X
+        
         X_mat = X.values # Превращаем из DataFrame в np array
         y_vec = y.values
         y_pred = X_mat @ self.weights
-        
+
+        errors = y_pred - y_vec
+        MSE = np.mean(errors**2)
+
+        # gradient = np.sum(2*(y_pred - y_vec) @ X_mat)
+        # gradient = 2*(y_pred - y_vec) @ X_mat
+        # print(gradient)
+
+        print("X после ввода нового столбца: \n", X, "\n")
+        print("self.weights после заполнения: \n", self.weights, "\n")
+        print("X_mat: \n", X_mat, "\n")
+        print("y_vec: \n", y_vec, "\n")
+        print("y_pred \n", y_pred, "\n")
+        print("errors \n", errors, "\n")
+        print("MSE \n", MSE, "\n")
+
 
 
 
@@ -60,4 +80,4 @@ y = pd.Series([1, 2, 3])
 verbose = 10
 MyLineReg1 = MyLineReg()
 print(MyLineReg1)
-MyLineReg1.fit(X, y, verbose)
+MyLineReg1.demo_fit(X, y, verbose)
