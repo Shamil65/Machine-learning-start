d6c8f42e59262178a7d0193356216c2112ccee44
Shamil65 <shamilziganshin132@gmail.com>
2026-02-13 23:28:23 +0500

    init fit

--- 2 Логистическая регрессия/2.0 Логистическая регрессия.py
+++ 2 Логистическая регрессия/2.0 Логистическая регрессия.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
 class MyLogReg():
     def __init__(self, n_iter=10, learning_rate=0.1):
         self.n_iter = n_iter
@@ -5,9 +9,10 @@ class MyLogReg():
 
     def __str__(self):
         return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
-        
 
-
+    def fit(self, X, y, verbose=False):
+        X_with_bias = X.copy()
+        X_with_bias.insert(0, "bias", 1)
 
 qqq = MyLogReg()
 
