49f7d13077aff75e19c6ce0dc942ab54ed4d56fa
Shamil65 <shamilziganshin132@gmail.com>
2026-02-25 23:43:21 +0500

    реализовал test_train_split version 2

--- ml_from_scratch/utils/data_processing.py
+++ ml_from_scratch/utils/data_processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 
-def _train_test_split(X, y, train_size=0.8, random_state=3310):
+def _train_test_split(X, y, train_size=0.8, random_state=3310, as_frame=False):
     # X, y - это датафреймы (dataframe)
 
     np.random.seed(random_state)
@@ -25,4 +25,7 @@ def _train_test_split(X, y, train_size=0.8, random_state=3310):
     X_test = test_data[:, :-1]
     y_test = test_data[:, -1]
 
-    return X_train, y_train, X_test, y_test
+    if as_frame:
+        return pd.DataFrame(X_train), pd.Series(y_train), pd.DataFrame(X_test), pd.Series(y_test)
+    else:
+        return X_train, y_train, X_test, y_test
